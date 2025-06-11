fe5fa2f5476bc78f3a01aa1b7748d4d19596d358
DevSkully <dev.skully01@gmail.com>
2025-06-11 23:56:01 +0800

    Changes in NavBar_Action
    not completed

--- odyssey/Scene/Player/NavBar_Action.gd
+++ odyssey/Scene/Player/NavBar_Action.gd
@@ -1,7 +1,15 @@
 class_name NavigationBar_Action
 extends Control
 
+signal set_HeaderText(String)
+signal set_TurnText(String)
+
 @onready var Nav : TextureRect = $TextureRect
+@onready var Header_Texts:Label = $HEADER/HeaderLabel
+@onready var Turn_Text : Label = $Header_Turn_Label/Label2
+
+func _ready() -> void:
+	pass
 
 func _process(delta: float) -> void:
 	pass
