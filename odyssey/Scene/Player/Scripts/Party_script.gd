6ad86a2b416c3c6ded39a0369cdaf0b07290f006
DevSkully <dev.skully01@gmail.com>
2025-06-21 06:50:51 +0800

    Subtle_Changes
    Party_System and on_Combat (Initial)

--- odyssey/Scene/Player/Scripts/Party_script.gd
+++ odyssey/Scene/Player/Scripts/Party_script.gd
@@ -5,14 +5,23 @@ signal Add_Character(Character)
 
 @onready var CharacterCards_Array : Array[Player_Card] = [ $"Player-Card_01",$"Player-Card_02",$"Player-Card_03"]
 
-var Characters : Dictionary
+var Character_Array:Array[Character]
+var index : int = 0
 
 func _ready() -> void:
 	Add_Character.connect(add_character_to_party)
 
 func add_character_to_party(value:Character)->void:
-	var index : int = 0
+	if index == 3 :
+		return
 	for card in CharacterCards_Array:
 		if !card.has_Character():
+			Character_Array.append(value)
 			card.Insert_Character(value)
 		++index
+
+func remove_character(value:Character)->void:
+	if index == 1:
+		return
+	if !Character_Array.has(value):
+		print("true")
--- odyssey/Scene/Player/battle_state.gd
+++ odyssey/Scene/Player/battle_state.gd
@@ -1,3 +1,15 @@
 extends Node
 
+
 var Active:bool = false
+
+func get_character(val:Character)->Array:
+	var arr : Array
+	arr.append(val.name)
+	arr.append(val.Dexterity)
+	return arr
+
+func get_party_matrix()->Array:
+	var matrix : Array
+	
+	return matrix
